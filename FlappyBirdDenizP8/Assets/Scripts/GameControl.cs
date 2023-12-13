7949250ce072cf1529510623e4948bbe5a813812
Aaron  Deniz <145504641+AaronDeniz@users.noreply.github.com>
2023-12-13 15:28:36 -0800

    Added moving sky
    i added a moving sky

--- FlappyBirdDenizP8/Assets/Scripts/GameControl.cs
+++ FlappyBirdDenizP8/Assets/Scripts/GameControl.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using JetBrains.Annotations;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake ()
@@ -20,14 +25,28 @@ public class GameControl : MonoBehaviour
         }
         else if (instance != this)
         {
-            Destroy(gameObject);
+            Destroy (gameObject);
         }
     }
 
     // Update is called once per frame
-    void Update() {
-
+    void Update() 
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+        }
     }
+        public void BirdScored()
+        {
+            if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+        }
+
         public void BirdDied()
         {
             gameOverText.SetActive(true);
